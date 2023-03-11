57ae8de4779256ce920562aa44b1f04553f826b0
박종우 <bagjong-u@bagjong-uui-MacBookPro.local>
2023-03-11 22:48:23 +0900

    #2 Search Result 화면 개발 시작

--- GithubRepositorySearchApp/GithubRepositorySearchApp/SearchViewModel.swift
+++ GithubRepositorySearchApp/GithubRepositorySearchApp/SearchViewModel.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import Combine
+
+struct SearchResultModel: Hashable {
+    var id: String = UUID().uuidString
+    
+    var title: String
+    
+    var logoImageURLString: String
+    
+    var logoTitle: String
+    
+    var description: String
+    
+    var imageCount: Int
+    
+    var language: String
+    
+    var languageColor: String
+}
+
+final class SearchViewModel: BaseViewModel {
+    
+    typealias Model = SearchResultModel
+    
+    var modelPublisher: PassthroughSubject<[SearchResultModel], Never> = .init()
+    
+    var alertPublisher: PassthroughSubject<AlertModel, Never> = .init()
+    
+    func viewLifeCycleUpdate(_ lifeCycle: BaseUILifeCycle) { }
+    
+}
