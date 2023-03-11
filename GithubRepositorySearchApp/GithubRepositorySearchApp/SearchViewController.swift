8396cda54562bdbcd59da8b23b1b41718ca48452
pinguding <41717656+pinguding@users.noreply.github.com>
2023-03-12 02:32:52 +0900

    첫번 째 Base Component 개발 완료 (#3)
    * #1 feat: Base Component 개발 시작
    
    * #1 Base ViewController 와 ViewModel 개발 진행
    
    ---------
    
    Co-authored-by: 박종우 <bagjong-u@bagjong-uui-MacBookPro.local>

---
+++ GithubRepositorySearchApp/GithubRepositorySearchApp/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubRepositorySearchApp
+//
+//  Created by 박종우 on 2023/03/11.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
+
---
+++ GithubRepositorySearchApp/GithubRepositorySearchApp/SearchViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SearchViewModel.swift
+//  GithubRepositorySearchApp
+//
+//  Created by 박종우 on 2023/03/11.
+//
+
+import Foundation
